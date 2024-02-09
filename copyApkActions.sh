a1f61f178ca0a4d7f23acf3d740bc513c911d9a6
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-02-09 22:01:06 +0200

    try to find path ыр

--- copyApkActions.sh
+++ copyApkActions.sh
@@ -5,17 +5,18 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+SOURCE="/Users/runner/work/android_github_mac_ubuntu/android_github_mac_ubuntu/android/app/build/outputs/apk/release/app-release.apk"
+DESTINATION="/Users/runner/work/android_github_mac_ubuntu/android_github_mac_ubuntu/app/android"
+
 echo -e "${GREEN}Copying apk...${NC}"
 
-APKDIR="$PWD/app/android"
+# Создаем целевую директорию, если она не существует
+mkdir -p "$DESTINATION"
 
-if [ -d "$APKDIR" ]; then
-    rm -r "$APKDIR"
-    echo -e "${RED}File removed.${NC}"    
+# Копируем файл APK
+if [ -f "$SOURCE" ]; then
+    cp "$SOURCE" "$DESTINATION/app-release.apk"
+    echo -e "${GREEN}Copy complete.${NC}"
+else
+    echo -e "${RED}Source file does not exist.${NC}"
 fi
-
-echo "Creating app directory..."
-mkdir -p $APKDIR
-cp -r /Users/runner/work/android_github_mac_ubuntu/android_github_mac_ubuntu/android/app/build/outputs/apk/release/app-release.apk $APKDIR
-
-echo -e "${GREEN}Copy complete.${NC}"

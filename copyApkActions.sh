e31e5584aa7b86a87d6381539a8ac05f317eb645
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-02-09 22:41:17 +0200

    add folders

--- copyApkActions.sh
+++ copyApkActions.sh
@@ -6,18 +6,15 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 SOURCE="/Users/runner/work/android_github_mac_ubuntu/android_github_mac_ubuntu/android/app/build/outputs/apk/release/app-release.apk"
+
 DESTINATION="/Users/runner/work/android_github_mac_ubuntu/android_github_mac_ubuntu/app/android"
+
 echo $SOURCE
 echo $DESTINATION
 echo -e "${GREEN}Copying apk...${NC}"
 
-# Создаем целевую директорию, если она не существует
-mkdir -p "$DESTINATION"
+mkdir -p $DESTINATION
 
-# Копируем файл APK
-if [ -f "$SOURCE" ]; then
-    cp "$SOURCE" "$DESTINATION/app-release.apk"
-    echo -e "${GREEN}Copy complete.${NC}"
-else
-    echo -e "${RED}Source file does not exist.${NC}"
-fi
+cp -r $SOURCE $DESTINATION
+
+echo -e "${GREEN}Copy complete.${NC}"

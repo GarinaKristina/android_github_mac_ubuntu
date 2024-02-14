26737d28650905714f1b81ab44d7e04a7b13c2cd
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-02-14 14:22:52 +0200

    await browser.pause(5000)

--- test/specs/openWebPage.e2e.ts
+++ test/specs/openWebPage.e2e.ts
@@ -4,6 +4,7 @@ import Actions from "../helpers/Actions.js";
 describe("My Login application", () => {
   it("should login with valid credentials", async () => {
     console.log("-------------", await browser.getPageSource());
+    await browser.pause(5000);
     await Actions.clickButton(HomePage.webButton);
   });
 });

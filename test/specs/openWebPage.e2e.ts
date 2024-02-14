d121e59a8699c511459818edc4e6a2028a971cb1
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-02-14 15:33:21 +0200

    { appId: "com.example.myapplication" });

--- test/specs/openWebPage.e2e.ts
+++ test/specs/openWebPage.e2e.ts
@@ -2,8 +2,16 @@ import HomePage from "../pageobjects/HomePage.js";
 import Actions from "../helpers/Actions.js";
 
 describe("My Login application", () => {
-  it("should login with valid credentials", async () => {
-    console.log("-------------", await browser.getPageSource());
+  it("try active app", async () => {
+    await driver.activateApp("com.example.myapplication");
+    console.log("-------------1-------------", await browser.getPageSource());
+  });
+  it("try active app 2", async () => {
+    await driver.execute("mobile: activateApp", { appId: "com.example.myapplication" });
+    console.log("-------------2-----------------", await browser.getPageSource());
+  });
+  it("should click", async () => {
+    console.log("-------------3---------------", await browser.getPageSource());
     await browser.pause(5000);
     await Actions.clickButton(HomePage.webButton);
   });
